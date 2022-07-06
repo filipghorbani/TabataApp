3519e3fe6a0067ea5783730831826c9a90701719
Filip Ghorbani <filip.ghorbani@consid.se>
2022-07-06 13:43:56 +0200

    Added FlatList

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+  const [items, setItems] = useState([
+    {id: 1, title: 'Title 1', time: '12:00', description: 'Description 1'},
+    {id: 2, title: 'Title 2', time: '12:00', description: 'Description 2'},
+    {id: 3, title: 'Title 3', time: '12:00', description: 'Description 3'},
+    {id: 4, title: 'Title 4', time: '12:00', description: 'Description 4'},
+    {id: 5, title: 'Title 5', time: '12:00', description: 'Description 5'},
+    {id: 6, title: 'Title 6', time: '12:00', description: 'Description 6'},
+    {id: 7, title: 'Title 7', time: '12:00', description: 'Description 7'},
+]);
+
+return (
+    <View style={styles.homeScreen}>
+
+        <FlatList
+            data={items}
+            renderItem={({ item }) => (
+                <View style={styles.item}>
+                    <View style={styles.image}>
+
+                    </View>
+                    <View style={styles.text}>
+                        <Text>{item.title}</Text>
+                        <Text>{item.description}</Text>
+                        <Text>{item.time}</Text>
+                    </View>
+                </View>
+            )}
+            keyExtractor={item => item.id}
+            contentContainerStyle={{ paddingVertical: 30 }}
+        />
     </View>
-  );
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
+homeScreen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: "white",
+},
+item: {
+    backgroundColor : "white",
+    height: 250,
+    marginTop: 30,
+    marginHorizontal: 20,
+    borderRadius: 8,
+    elevation: 7
+},
+image: {
+    backgroundColor: "pink",
+    flex: 2
+},
+text: {
+    flex: 1,
+    padding: 10,
+}
+
+})
