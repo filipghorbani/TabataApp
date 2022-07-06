1caa7112fa0868e01ed625eb54b9e87be33109c9
Filip Ghorbani <filip.ghorbani@consid.se>
2022-07-06 14:08:23 +0200

    Prettier

--- App.js
+++ App.js
@@ -1,64 +1,70 @@
-import { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableWithoutFeedback } from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 export default function App() {
-
   const [items, setItems] = useState([
-    {id: 1, title: 'Title 1', time: '12:00', description: 'Description 1'},
-    {id: 2, title: 'Title 2', time: '12:00', description: 'Description 2'},
-    {id: 3, title: 'Title 3', time: '12:00', description: 'Description 3'},
-    {id: 4, title: 'Title 4', time: '12:00', description: 'Description 4'},
-    {id: 5, title: 'Title 5', time: '12:00', description: 'Description 5'},
-    {id: 6, title: 'Title 6', time: '12:00', description: 'Description 6'},
-    {id: 7, title: 'Title 7', time: '12:00', description: 'Description 7'},
-]);
+    { id: 1, title: "Title 1", time: "12:00", description: "Description 1" },
+    { id: 2, title: "Title 2", time: "12:00", description: "Description 2" },
+    { id: 3, title: "Title 3", time: "12:00", description: "Description 3" },
+    { id: 4, title: "Title 4", time: "12:00", description: "Description 4" },
+    { id: 5, title: "Title 5", time: "12:00", description: "Description 5" },
+    { id: 6, title: "Title 6", time: "12:00", description: "Description 6" },
+    { id: 7, title: "Title 7", time: "12:00", description: "Description 7" },
+  ]);
 
-return (
+  return (
     <View style={styles.homeScreen}>
-
-        <FlatList
-            data={items}
-            renderItem={({ item }) => (
-              <TouchableWithoutFeedback onPress={() => {alert(item.title)}}>
-                <View style={styles.item}>
-                    <View style={styles.image}>
-
-                    </View>
-                    <View style={styles.text}>
-                        <Text>{item.title}</Text>
-                        <Text>{item.description}</Text>
-                        <Text>{item.time}</Text>
-                    </View>
-                </View>
-              </TouchableWithoutFeedback>
-            )}
-            keyExtractor={item => item.id}
-            contentContainerStyle={{ paddingVertical: 30 }}
-        />
+      <FlatList
+        data={items}
+        renderItem={({ item }) => (
+          <TouchableWithoutFeedback
+            onPress={() => {
+              alert(item.title);
+            }}
+          >
+            <View style={styles.item}>
+              <View style={styles.image}></View>
+              <View style={styles.text}>
+                <Text>{item.title}</Text>
+                <Text>{item.description}</Text>
+                <Text>{item.time}</Text>
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+        )}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{ paddingVertical: 30 }}
+      />
     </View>
-);
+  );
 }
 
 const styles = StyleSheet.create({
-homeScreen: {
+  homeScreen: {
     flex: 1,
     backgroundColor: "white",
-},
-item: {
-    backgroundColor : "white",
+  },
+  item: {
+    backgroundColor: "white",
+    overflow: "hidden",
     height: 250,
     marginTop: 30,
     marginHorizontal: 20,
-    borderRadius: 8,
-    elevation: 7
-},
-image: {
-    backgroundColor: "pink",
-    flex: 2
-},
-text: {
-    flex: 1,
-    padding: 10,
-}
-
-})
+    borderRadius: 6,
+    elevation: 7,
+  },
+  image: {
+    backgroundColor: "peachpuff",
+    flex: 2,
+  },
+  text: {
+    flex: 1.5,
+    margin: 10,
+  },
+});
